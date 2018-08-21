8234ca5c0be829621d4d1bcf4feed4d0bcfd8aae
Jenny Plunkett <jennifer.plunkett@arm.com>
2018-08-21 15:01:48 -0500

    Bug fixes

--- bot.js
+++ bot.js
@@ -6,13 +6,15 @@ var msgs = ["Maybe yes. Maybe no. Maybe go fuck yourself.",
             "Technically correct. The only kind of correct that matters.",
             "Yeetus that fetus.",
             "E",
-            "*clattering background noise* (afk)"];
+            "*clattering background noise* (afk)",
+            "Citation needed"];
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   client.user.setActivity(`the auction house`);
   client.guilds.forEach((guild) => {
+    guild.defaultChannel.send("I am Max. beep boop beep").catch(console.error);
     setInterval (function () {
       var message = msgs[Math.floor(Math.random()*msgs.length)];
       console.log(message);
